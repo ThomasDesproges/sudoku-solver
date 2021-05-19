57f64fded88d5a7af069ccc185105b5f7cf423a4
Tom Bertheas <tom.bertheas@orange.fr>
2021-05-19 21:12:13 +0200

    petits réglages sur le premier code de camera, rapide prise en main de PyCharm

--- App.js
+++ App.js
@@ -1,245 +1,39 @@
+import {StatusBar} from "expo-status-bar";
+import React, {useState, useEffect} from "react";
+import {StyleSheet, Text, View, TouchableOpacity, ImageBackground} from "react-native";
+import {Camera} from "expo-camera";
 
-/*import React from "react";
-import { StyleSheet, Text, View} from "react-native";
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}*/
-
-/*import React, { useState, useEffect, useRef } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
-import { Camera } from 'expo-camera';
+const tag = "[CAMERA]";
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
-  const [cameraRef, setCameraRef] = useState(null)
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [capturedImage, setCapturedImage] = useState(null);
+  const [startOver, setStartOver] = useState(true);
   const [type, setType] = useState(Camera.Constants.Type.back);
-useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
-if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
-  return (
-    <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={type} ref={ref => {
-        setCameraRef(ref) ;
-  }}>
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'transparent',
-            justifyContent: 'flex-end'
-          }}>
-          <TouchableOpacity
-            style={{
-              flex: 0.1,
-              alignSelf: 'flex-end'
-            }}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}>
-            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={{alignSelf: 'center'}} onPress={async() => {
-            if(cameraRef){
-              let photo = await cameraRef.takePictureAsync();
-              console.log('photo', photo);
-            }
-          }}>
-            <View style={{ 
-               borderWidth: 2,
-               borderRadius:"50%",
-               borderColor: 'white',
-               height: 50,
-               width:50,
-               display: 'flex',
-               justifyContent: 'center',
-               alignItems: 'center'}}
-            >
-              <View style={{
-                 borderWidth: 2,
-                 borderRadius:"50%",
-                 borderColor: 'white',
-                 height: 40,
-                 width:40,
-                 backgroundColor: 'white'}} >
-              </View>
-            </View>
-          </TouchableOpacity>
-        </View>
-      </Camera>
-    </View>
-  );
-}*/
 
-/*import {StatusBar} from 'expo-status-bar'
-import React from 'react'
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native'
-import {Camera} from 'expo-camera'
-let camera: Camera
-
-
-export default function App() {
-  const [startCamera,setStartCamera] = React.useState(false)
-
-  const __startCamera = async () => {
-    const {status} = await Camera.requestPermissionsAsync()
-    if (status === 'granted') {
-      // start the camera
-      setStartCamera(true)
-    } else {
-      Alert.alert('Access denied')
-    }
-  }
-
-  const __takePicture = async () => {
-    if (!camera) return
-    const photo = await camera.takePictureAsync()
-    console.log(photo)
-    setPreviewVisible(true)
-    setCapturedImage(photo)
-  }
-
-
-
-  return (
-    <View style={styles.container}>
-      {startCamera ? (
-        <Camera
-          style={{flex: 1,width:"100%"}}
-          ref={(r) => {
-            camera = r
-          }}
-        >
-         <View
-        style={{
-        position: 'absolute',
-        bottom: 0,
-        flexDirection: 'row',
-        flex: 1,
-        width: '100%',
-        padding: 20,
-        justifyContent: 'space-between'
-        }}
-        >
-        <View
-        style={{
-        alignSelf: 'center',
-        flex: 1,
-        alignItems: 'center'
-        }}
-        >
-            <TouchableOpacity
-            onPress={__takePicture}
-            style={{
-            width: 70,
-            height: 70,
-            bottom: 30,
-            borderRadius: 50,
-            backgroundColor: '#fff'
-            }}
-            />
-        </View>
-        </View>
-        </Camera>
-      ) : (
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: '#fff',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <TouchableOpacity
-            onPress={__startCamera}
-            style={{
-              width: 200,
-              borderRadius: 4,
-              backgroundColor: 'green',
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: 50
-            }}
-          >
-            <Text
-              style={{
-                color: '#fff',
-                fontWeight: 'bold',
-                textAlign: 'center'
-              }}
-            >
-              Take picture
-            </Text>
-          </TouchableOpacity>
-        </View>
-      )}
-      </View>
-  )}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})*/
-
-import {StatusBar} from 'expo-status-bar'
-import React, {useState, useEffect} from 'react'
-import {StyleSheet, Text, View, TouchableOpacity, ImageBackground} from 'react-native'
-import {Camera} from 'expo-camera'
-const tag = '[CAMERA]'
-export default function App() {
-  const [hasPermission, setHasPermission] = useState(null)
-  const [previewVisible, setPreviewVisible] = useState(false)
-  const [capturedImage, setCapturedImage] = useState(null)
-  const [startOver, setStartOver] = useState(true)
-  const [type, setType] = useState(Camera.Constants.Type.back)
-  let camera: Camera
+  let camera: Camera;
   useEffect(() => {
     ;(async () => {
-      const {status} = await Camera.requestPermissionsAsync()
-      setHasPermission(status === 'granted')
-    })()
-  }, [])
+      const {status} = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
   const __closeCamera = () => {
-    setStartOver(true)
-  }
+    setStartOver(true);
+  };
+
   const __takePicture = async () => {
-    if (!camera) return
-    const photo = await camera.takePictureAsync()
-    console.log(photo)
-    setPreviewVisible(true)
-    setCapturedImage(photo)
-  }
-  const __savePhoto = async () => {}
+    if (!camera) return;
+    const photo = await camera.takePictureAsync();
+    console.log(photo);
+    setPreviewVisible(true);
+    setCapturedImage(photo);
+  };
+
+  const __savePhoto = async () => {};
+
   return (
     <View
       style={{
@@ -250,9 +44,9 @@ export default function App() {
         <View
           style={{
             flex: 1,
-            backgroundColor: '#fff',
-            justifyContent: 'center',
-            alignItems: 'center'
+            backgroundColor: "#fff",
+            justifyContent: "center",
+            alignItems: "center"
           }}
         >
           <TouchableOpacity
@@ -260,18 +54,18 @@ export default function App() {
             style={{
               width: 130,
               borderRadius: 4,
-              backgroundColor: '#14274e',
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
+              backgroundColor: "#14274e",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
               height: 40
             }}
           >
             <Text
               style={{
-                color: '#fff',
-                fontWeight: 'bold',
-                textAlign: 'center'
+                color: "#fff",
+                fontWeight: "bold",
+                textAlign: "center"
               }}
             >
               Take picture
@@ -294,15 +88,15 @@ export default function App() {
               <View
                 style={{
                   flex: 1,
-                  flexDirection: 'column',
+                  flexDirection: "column",
                   padding: 15,
-                  justifyContent: 'flex-end'
+                  justifyContent: "flex-end"
                 }}
               >
                 <View
                   style={{
-                    flexDirection: 'row',
-                    justifyContent: 'space-between'
+                    flexDirection: "row",
+                    justifyContent: "space-between"
                   }}
                 >
                   <TouchableOpacity
@@ -311,13 +105,13 @@ export default function App() {
                       width: 130,
                       height: 40,
 
-                      alignItems: 'center',
+                      alignItems: "center",
                       borderRadius: 4
                     }}
                   >
                     <Text
                       style={{
-                        color: '#fff',
+                        color: "#fff",
                         fontSize: 20
                       }}
                     >
@@ -330,13 +124,13 @@ export default function App() {
                       width: 130,
                       height: 40,
 
-                      alignItems: 'center',
+                      alignItems: "center",
                       borderRadius: 4
                     }}
                   >
                     <Text
                       style={{
-                        color: '#fff',
+                        color: "#fff",
                         fontSize: 20
                       }}
                     >
@@ -351,27 +145,27 @@ export default function App() {
               style={{flex: 1}}
               type={type}
               ref={(r) => {
-                camera = r
+                camera = r;
               }}
             >
               <View
                 style={{
                   flex: 1,
-                  backgroundColor: 'transparent',
-                  flexDirection: 'row'
+                  backgroundColor: "transparent",
+                  flexDirection: "row"
                 }}
               >
                 <View
                   style={{
-                    position: 'absolute',
-                    top: '5%',
-                    right: '5%'
+                    position: "absolute",
+                    top: "5%",
+                    right: "5%"
                   }}
                 >
                   <TouchableOpacity onPress={__closeCamera}>
                     <Text
                       style={{
-                        color: '#fff',
+                        color: "#fff",
                         fontSize: 40
                       }}
                     >
@@ -381,32 +175,32 @@ export default function App() {
                 </View>
                 <TouchableOpacity
                   style={{
-                    position: 'absolute',
-                    top: '5%',
-                    left: '5%'
+                    position: "absolute",
+                    top: "5%",
+                    left: "5%"
                   }}
                   onPress={() => {
-                    setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back)
+                    setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back);
                   }}
                 >
-                  <Text style={{fontSize: 18, marginBottom: 10, color: 'white'}}> Flip </Text>
+                  <Text style={{fontSize: 18, marginBottom: 10, color: "white"}}> Flip </Text>
                 </TouchableOpacity>
                 <View
                   style={{
-                    position: 'absolute',
+                    position: "absolute",
                     bottom: 0,
-                    flexDirection: 'row',
+                    flexDirection: "row",
                     flex: 1,
-                    width: '100%',
+                    width: "100%",
                     padding: 20,
-                    justifyContent: 'space-between'
+                    justifyContent: "space-between"
                   }}
                 >
                   <View
                     style={{
-                      alignSelf: 'center',
+                      alignSelf: "center",
                       flex: 1,
-                      alignItems: 'center'
+                      alignItems: "center"
                     }}
                   >
                     <TouchableOpacity
@@ -416,7 +210,7 @@ export default function App() {
                         height: 70,
                         bottom: 0,
                         borderRadius: 50,
-                        backgroundColor: '#fff'
+                        backgroundColor: "#fff"
                       }}
                     />
                   </View>
@@ -427,14 +221,14 @@ export default function App() {
         </View>
       )}
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center"
   }
-})
+});
