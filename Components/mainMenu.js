e042b8b6954f0bd873425995599a0693c8c0978b
Tom Bertheas <tom.bertheas@orange.fr>
2021-06-17 16:57:25 +0200

    main menu screen

--- Components/mainMenu.js
+++ Components/mainMenu.js
@@ -4,39 +4,48 @@ import {Text, TouchableOpacity, View} from "react-native";
 function mainMenu(setStartOver) {
     return (
         <View
-          style={{
-            flex: 1,
-            backgroundColor: "#fff",
-            justifyContent: "center",
-            alignItems: "center"
-          }}
-        >
-          <View style={{
-            flex: 1,
-            backgroundColor: "#fff",
-            justifyContent: "center",
-            alignItems: "center",
-          }}>
-            <Text style={{color: "#000", fontSize: 40, fontWeight: "bold"}}> Sudoku </Text>
-            <Text style={{color: "#000", fontSize: 40, fontWeight: "bold"}}> Solver </Text>
-          </View>
-
-          <TouchableOpacity
-            onPress={() => setStartOver(false)}
             style={{
-              width: 130,
-              borderRadius: 4,
-              backgroundColor: "#14274e",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-              height: 40,
+                flex: 1,
+                backgroundColor: "#fff",
+                alignItems: "center",
+                justifyItems: "space-evenly",
             }}
-          >
+        >
+            <View style={{
+                flex: 3/5,
+                justifyContent: "center",
+            }}>
+                <Text style={{color: "#000", fontSize: 40, fontWeight: "bold", textAlign:"center"}}> Sudoku </Text>
+                <Text style={{color: "#000", fontSize: 40, fontWeight: "bold", textAlign:"center"}}> Solver </Text>
+            </View>
 
-            <Text style={{color: "#fff", fontWeight: "bold", textAlign: "center"}}> Take picture </Text>
+            <TouchableOpacity
+                onPress={() => setStartOver(false)}
+                style={{
+                    width: 130,
+                    borderRadius: 4,
+                    backgroundColor: "#14274e",
+                    justifyContent: "center",
+                    margin: 10,
+                    height: 40,
+                }}
+            >
+                <Text style={{color: "#fff", fontWeight: "bold", textAlign: "center"}}> Take picture </Text>
+            </TouchableOpacity>
 
-          </TouchableOpacity>
+            <TouchableOpacity
+                onPress={() => setStartOver(false)}
+                style={{
+                    width: 130,
+                    borderRadius: 4,
+                    backgroundColor: "#14274e",
+                    justifyContent: "center",
+                    margin: 10,
+                    height: 40,
+                }}
+            >
+                <Text style={{color: "#fff", fontWeight: "bold", textAlign: "center"}}> Display grid </Text>
+            </TouchableOpacity>
 
         </View>
     );
