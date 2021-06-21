284b05b4d05ff29f7f58388df1e8f50db415ad98
Alexandre Grenier <alexandre.grenier@mines-paristech.fr>
2021-06-21 11:29:17 +0200

    model ocr machine learning python

---
+++ test-ocr-python/digit.py
@@ -0,0 +1,51 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import scipy.io as sio
+
+mnist = tf.keras.datasets.mnist
+(x_train,y_train),(x_test,y_test)= mnist.load_data()
+
+x_train = tf.keras.utils.normalize(x_train, axis = 1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=3)
+
+loss, accuracy = model.evaluate(x_test, y_test)
+print(accuracy)
+print(loss)
+
+
+model.save('digits.model')
+succes = 0
+for i in range(1,10):
+    img = cv.imread('paint/' + str(i) + '.png')[:,:,0]
+    img = np.invert(np.array([img]))
+    #img = np.array([img])
+    prediction = model.predict(img)
+    print("Nombre : " + str(i) + " - Prediction : " + str(np.argmax(prediction)))
+    if(i==np.argmax(prediction)):
+        succes+=1
+
+
+for i in range(1,10):
+    img = cv.imread(str(i) + '.png')[:,:,0]
+    #img = np.invert(np.array([img]))
+    img = np.array([img])
+    prediction = model.predict(img)
+    print("Nombre : " + str(i) + " - Prediction : " + str(np.argmax(prediction)))
+    if(i==np.argmax(prediction)):
+        succes+=1
+
+print("------------")
+print("Precision : " + str(int(succes*100/18)) + "%")
+
